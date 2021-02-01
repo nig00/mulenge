a3ad309bad9ee9a0450c203e5e8d8e01ab46fdf2
nig00 <pfizpatrick@gmail.com>
2021-02-01 16:55:24 +0000

    Create Ascui converter.cpp

---
+++ Ascui converter.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    // Ascui Characters converted into numerical values
+using namespace std;
+ char asc;
+   cout << "Enter Ascui Character[Example : a,b,c ] : ";
+   cin>>asc;
+   cout <<"the character number = "<<int (asc) <<endl;
+ 
+    return 0;
+}
